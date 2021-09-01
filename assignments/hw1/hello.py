0ec17191b06b0e29f3e5078eeef33ce0d116042b
JustinL <luftjn@g.cofc.edu>
2021-09-01 15:12:05 -0400

    Justin Luft Completed HW1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+"""
+Justin Luft
+Hello.py
+
+This program should print "hello, world!" to the screen
+
+I certify that this code is written by me, Justin Luft
+"""
+def main():
+    print("hello, world!")
+main()
