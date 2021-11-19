ebce71fca1df77d6acc5d31a482b57410fd63f91
JustinL <luftjn@g.cofc.edu>
2021-11-18 21:49:56 -0500

    Justin Luft HW 9

--- assignments/hw9/button.py
+++ assignments/hw9/button.py
@@ -6,25 +6,48 @@ class Button:
     def __init__(self, shape, text):
         self.shape = shape
         self.text = text
+        rec1 = shape.getP1()
+        p1x = rec1.getX()
+        p1y = rec1.getY()
+        rec2 = shape.getP2()
+        p2x = rec2.getX()
+        p2y = rec2.getY()
+        centerxbef = p1x + p2x
+        centerx = centerxbef / 2
+        centerybef = p1y + p2y
+        centery = centerybef / 2
+        textbox = Text(Point(centerx, centery), self.text)
+        self.textbox = textbox
+
     def get_label(self):
-        text = self.text
+        text = self.textbox
         text = text.getText()
         return text
 
     def draw(self, win):
         clickable = self.shape
         clickable.draw(win)
-        self.clickable = clickable
-        texty = self.text
-        texty.draw(win)
+        textbox = self.textbox
+        textbox.draw(win)
+        self.textbox = textbox
 
     def undraw(self):
-        shape = self.clickable
+        shape = self.shape
         text = self.textbox
         shape.undraw()
         text.undraw()
 
+    def set_label(self, label):
+        newtext = self.textbox
+        newtext.setText(label)
+
+    def color_button(self, color):
+        clickable = self.shape
+        clickable.setFill(str(color))
+
     def is_clicked(self, mouseclick):
+        mouseclickx = mouseclick.getX()
+        mouseclicky = mouseclick.getY()
         shape = self.shape
         rec1 = shape.getP1()
         p1x = rec1.getX()
@@ -32,8 +55,6 @@ class Button:
         rec2 = shape.getP2()
         p2x = rec2.getX()
         p2y = rec2.getY()
-        mouseclickx = mouseclick.getX()
-        mouseclicky = mouseclick.getY()
         if p1x >= p2x:
             if mouseclickx <= p1x and mouseclickx >= p2x:
                 if p1y >= p2y:
@@ -51,25 +72,3 @@ class Button:
                     if mouseclicky <= p1y and mouseclicky >= p2y:
                         return True
         return False
-
-    def color_button(self, color):
-        clickable = self.shape
-        clickable.setFill(str(color))
-
-    def set_label(self, label):
-        newtext = self.text
-        newtext.setText(label)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- assignments/hw9/three_door_game.py
+++ assignments/hw9/three_door_game.py
@@ -6,64 +6,64 @@ I certify that this code is entirely written by me
 from random import *
 from button import *
 from graphics import *
-winner = randint(1, 3)
-win = GraphWin("Three Door Game", 500, 500)
-Box1 = Rectangle(Point(340, 275), Point(455, 350))
-textbox = Text(Point(397.5, 312.5), "Button 1")
-Box1 = Button(Box1, textbox)
-Box1.draw(win)
-Box2 = Rectangle(Point(200, 275), Point(315, 350))
-textbox2 = Text(Point(257.5, 312.5), "Button 2")
-Box2 = Button(Box2, textbox2)
-Box2.draw(win)
-Box3 = Rectangle(Point(60, 275), Point(175, 350))
-textbox3 = Text(Point(117.5, 312.5), "Button 3")
-Box3 = Button(Box3, textbox3)
-Box3.draw(win)
-guider = Text(Point(250, 50), "I have a secret door")
-guider.draw(win)
-rules = Text(Point(250, 450), "Click to choose my door")
-rules.draw(win)
-theguess = win.getMouse()
-if winner == 3:
-    if Box1.is_clicked(theguess):
-        Box1.color_button("lime")
-        guider.setText("You Win!")
-        rules.setText("Click to close")
-    elif Box2.is_clicked(theguess):
-        Box2.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-    elif Box3.is_clicked(theguess):
-        Box3.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-if winner == 2:
-    if Box1.is_clicked(theguess):
-        Box1.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-    elif Box2.is_clicked(theguess):
-        Box2.color_button("lime")
-        guider.setText("You Win!")
-        rules.setText("Click to close")
-    elif Box3.is_clicked(theguess):
-        Box3.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-if winner == 1:
-    if Box1.is_clicked(theguess):
-        Box1.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-    elif Box2.is_clicked(theguess):
-        Box2.color_button("red")
-        guider.setText("You Lost!")
-        rules.setText("Door " + str(winner) + " was my secret door!")
-    elif Box3.is_clicked(theguess):
-        Box3.color_button("lime")
-        guider.setText("You Win!")
-        rules.setText("Click to close")
+def main():
+    winner = randint(1, 3)
+    win = GraphWin("Three Door Game", 500, 500)
+    box1 = Rectangle(Point(340, 275), Point(455, 350))
+    box1 = Button(box1, "Box 3")
+    box1.draw(win)
+    box2 = Rectangle(Point(200, 275), Point(315, 350))
+    box2 = Button(box2, "Box 2")
+    box2.draw(win)
+    box3 = Rectangle(Point(60, 275), Point(175, 350))
+    box3 = Button(box3, "Box 1")
+    box3.draw(win)
+    guider = Text(Point(250, 50), "I have a secret door")
+    guider.draw(win)
+    rules = Text(Point(250, 450), "Click to choose my door")
+    rules.draw(win)
+    theguess = win.getMouse()
+    if winner == 3:
+        if box1.is_clicked(theguess):
+            box1.color_button("lime")
+            guider.setText("You Win!")
+            rules.setText("Click to close")
+        elif box2.is_clicked(theguess):
+            box2.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+        elif box3.is_clicked(theguess):
+            box3.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+    if winner == 2:
+        if box1.is_clicked(theguess):
+            box1.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+        elif box2.is_clicked(theguess):
+            box2.color_button("lime")
+            guider.setText("You Win!")
+            rules.setText("Click to close")
+        elif box3.is_clicked(theguess):
+            box3.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+    if winner == 1:
+        if box1.is_clicked(theguess):
+            box1.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+        elif box2.is_clicked(theguess):
+            box2.color_button("red")
+            guider.setText("You Lost!")
+            rules.setText("Door " + str(winner) + " was my secret door!")
+        elif box3.is_clicked(theguess):
+            box3.color_button("lime")
+            guider.setText("You Win!")
+            rules.setText("Click to close")
 
-win.getMouse()
-win.close()
+    win.getMouse()
+    win.close()
+if __name__ == '__main__':
+    main()
