b17cc847959555eb55195cd863408f92014b1a8c
JustinL <luftjn@g.cofc.edu>
2021-11-18 21:35:41 -0500

    Justin Luft HW 9

--- assignments/hw9/button.py
+++ assignments/hw9/button.py
@@ -5,28 +5,18 @@ from graphics import *
 class Button:
     def __init__(self, shape, text):
         self.shape = shape
-        rec1 = shape.getP1()
-        self.p1x = rec1.getX()
-        self.p1y = rec1.getY()
-        rec2 = shape.getP2()
-        self.p2x = rec2.getX()
-        self.p2y = rec2.getY()
         self.text = text
     def get_label(self):
         text = self.text
+        text = text.getText()
         return text
+
     def draw(self, win):
         clickable = self.shape
         clickable.draw(win)
         self.clickable = clickable
-        centerxbef = self.p1x + self.p2x
-        centerx = centerxbef / 2
-        centerybef = self.p1y + self.p2y
-        centery = centerybef / 2
-        textbox = Text(Point(centerx, centery), self.text)
-        textbox.draw(win)
-        self.textbox = textbox
-
+        texty = self.text
+        texty.draw(win)
 
     def undraw(self):
         shape = self.clickable
@@ -34,34 +24,45 @@ class Button:
         shape.undraw()
         text.undraw()
 
-    def set_label(self, label):
-        newtext = self.textbox
-        newtext.setText(label)
+    def is_clicked(self, mouseclick):
+        shape = self.shape
+        rec1 = shape.getP1()
+        p1x = rec1.getX()
+        p1y = rec1.getY()
+        rec2 = shape.getP2()
+        p2x = rec2.getX()
+        p2y = rec2.getY()
+        mouseclickx = mouseclick.getX()
+        mouseclicky = mouseclick.getY()
+        if p1x >= p2x:
+            if mouseclickx <= p1x and mouseclickx >= p2x:
+                if p1y >= p2y:
+                    if mouseclicky <= p1y and mouseclicky >= p2y:
+                        return True
+                elif p2y >= p1y:
+                    if mouseclicky < p2y and mouseclicky > p1y:
+                        return True
+        elif p2x >= p1x:
+            if mouseclickx <= p2x and mouseclickx >= p1x:
+                if p2y >= p1y:
+                    if mouseclicky <= p2y and mouseclicky >= p1y:
+                        return True
+                elif p1y >= p2y:
+                    if mouseclicky <= p1y and mouseclicky >= p2y:
+                        return True
+        return False
 
     def color_button(self, color):
         clickable = self.shape
         clickable.setFill(str(color))
 
-    def is_clicked(self, mouseclick):
-        mouseclickx = mouseclick.getX()
-        mouseclicky = mouseclick.getY()
-        if self.p1x >= self.p2x:
-            if mouseclickx <= self.p1x and mouseclickx >= self.p2x:
-                if self.p1y >= self.p2y:
-                    if mouseclicky <= self.p1y and mouseclicky >= self.p2y:
-                        return True
-                elif self.p2y >= self.p1y:
-                    if mouseclicky < self.p2y and mouseclicky > self.p1y:
-                        return True
-        elif self.p2x >= self.p1x:
-            if mouseclickx <= self.p2x and mouseclickx >= self.p1x:
-                if self.p2y >= self.p1y:
-                    if mouseclicky <= self.p2y and mouseclicky >= self.p1y:
-                        return True
-                elif self.p1y >= self.p2y:
-                    if mouseclicky <= self.p1y and mouseclicky >= self.p2y:
-                        return True
-        return False
+    def set_label(self, label):
+        newtext = self.text
+        newtext.setText(label)
+
+
+
+
 
 
 
