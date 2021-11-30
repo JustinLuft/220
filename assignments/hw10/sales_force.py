d5db7c02f5fead6ba48ef574e19b01a78c449fd3
JustinL <luftjn@g.cofc.edu>
2021-11-29 21:43:51 -0500

    Justin Luft HW 10

---
+++ assignments/hw10/sales_force.py
@@ -0,0 +1,77 @@
+"""
+sales_force.py
+I certify that this code is written by me, Justin Luft
+"""
+from sales_person import SalesPerson
+class SalesForce:
+    def __init__(self):
+        slist = []
+        mlist = []
+        infile = open("salesData.txt", "r")
+        for line in infile:
+            line = line.split(",")
+            saleperson = SalesPerson(int(line[0]), str(line[1]))
+            slist = saleperson.get_id(), saleperson.get_name(), saleperson.get_sales()
+            mlist.append(slist)
+            self.sales_people = mlist
+
+    def add_data(self, file_name):
+        infile = open(file_name, "r")
+        slist = []
+        for line in infile:
+            line = line.split(",")
+            saleperson = SalesPerson(int(line[0]), str(line[1]))
+            slist = saleperson.get_id(), saleperson.get_name(), saleperson.get_sales()
+            self.sales_people.append(slist)
+
+    def quota_report(self, quota):
+        qlist = []
+        infile = open("salesData.txt", "r")
+        for line in infile:
+            line = line.split(",")
+            saleperson = SalesPerson(int(line[0]), str(line[1]))
+            if saleperson.met_quota(quota) == True:
+                slist = saleperson.get_id(), saleperson.get_name(), saleperson.get_sales(), True
+            else:
+                slist = saleperson.get_id(), saleperson.get_name(), saleperson.get_sales(), False
+            qlist.append(slist)
+        return qlist
+
+    def top_seller(self):
+        tlist = []
+        slist = []
+        mlist = []
+        infile = open("salesData.txt", "r")
+        for line in infile:
+            line = line.split(",")
+            saleperson = SalesPerson(int(line[0]), str(line[1]))
+            slist.append(str(line[1]))
+            total = saleperson.total_sales()
+            tlist.append(total)
+        maxnum = max(tlist)
+        acc = 0
+        for i in tlist:
+            if i == maxnum:
+                mlist.append(slist[acc])
+                acc = acc + 1
+            else:
+                acc = acc + 1
+        return mlist
+    def individual_sales(self, employee_id):
+        infile = open("salesData.txt", "r")
+        for line in infile:
+            line = line.split(",")
+            if str(line[0]) == str(employee_id):
+                print(SalesPerson(line[0], line[1]))
+                return SalesPerson(line[0], line[1])
+        return None
+
+
+
+
+
+
+
+
+
+
---
+++ assignments/hw10/sales_person.py
@@ -0,0 +1,54 @@
+"""
+sales_person.py
+SalesPerson class
+I certify that this code is written by me, Justin Luft
+"""
+
+class SalesPerson:
+    def __init__(self, employee_id, name):
+        self.employee_id = employee_id
+        self.name = name
+        self.sales = []
+    def get_id(self):
+        return self.employee_id
+    def get_name(self):
+        return self.name
+    def set_name(self, name):
+        self.name = name
+    def enter_sale(self, sale):
+        self.sales.append(sale)
+    def total_sales(self):
+        acc = 0
+        sales = self.sales
+        for i in self.sales:
+            acc = acc + i
+        return float(acc)
+    def get_sales(self):
+        return self.sales
+    def met_quota(self, quota):
+        total = 0
+        for i in self.sales:
+            total = total + i
+        print(total)
+        if total >= quota:
+            return True
+        else:
+            return False
+    def compare_to(self, other):
+        total = 0
+        sales = self.sales
+        for i in self.sales:
+            total = total + i
+        salesy = other.total_sales()
+        if salesy > total:
+            return -1
+        elif total > salesy:
+            return 1
+        elif total == salesy:
+            return 0
+    def __str__(self):
+        total = 0
+        for i in self.sales:
+            total = total + i
+        returnme = str(self.employee_id) + "-" + str(self.name) + ":" + str(total)
+        return returnme
